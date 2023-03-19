36549db0d80ce7f755308b95b21fe79d24dea8ec
Dawid Pieczykolan <dawid125w36@outlook.com>
2023-03-19 20:54:45 +0100

    Dodaj pliki projektów.

---
+++ Karta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa
+{
+    public class Karta
+    {
+        List<float> oceny;
+
+        public void Dodajocene(float ocena) 
+        {
+            oceny.Add(ocena);
+        }
+        public float Obliczsrednia() 
+        {
+          return oceny.Average();
+        }
+        public float NajniszaOcena() 
+        {
+            return oceny.Min();  
+        }
+        public float NajwyszaOcena() 
+        {
+        return  oceny.Max();
+        }
+
+    }
+}
